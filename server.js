801991f6a3eeb6f6fae5b560db43c24b4d16cfec
adeebkhanjar <adeeb.kh93@gmail.com>
2021-12-08 19:27:34 +0200

    second commit

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ app.use(express.json())
 app.use(cors());
 
 app.use('/api/users', require('./routes/users.route'));
-
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    app.use(express.static('client/build'));
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 mongoose.connect(`${process.env.DB_URL}`, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log('Connected to DB');
 });
